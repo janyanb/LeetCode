eaa022165347dae428b265b61742646181a2eec3
Janya NB <janya-cs060@dsce.edu.in>
2025-02-13 16:36:12 -0800

    Update 19129-770-152-maximum-product-subarray.cpp

--- 19129-770-152-maximum-product-subarray/19129-770-152-maximum-product-subarray.cpp
+++ 19129-770-152-maximum-product-subarray/19129-770-152-maximum-product-subarray.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        auto it = max_element(nums.begin(), nums.end());
+
+        //O(n) approach usind DP
+        /*auto it = max_element(nums.begin(), nums.end());
         int result;
         if(it != nums.end()){
             result = *it;
@@ -18,6 +20,30 @@ public:
         currMin = min({n, tmp, currMin*n});
         result = max(result, currMax);
         }
-        return result; 
+        return result;*/
+
+        //O (n^2) using sliding window
+        int left = 0, right = 0;
+        int result, prod = 1;
+        auto maximum = max_element(nums.begin(), nums.end());
+        if(maximum != nums.end()){
+            result = *maximum;
+        };
+
+        while(right < nums.size()){
+            for(int curr = left; curr <= right; curr++){
+                prod *= nums[curr];
+            }
+            result = max(result, prod);
+            right++;
+            prod = 1;
+            if(right == nums.size()){
+                left ++;
+                right = left;
+            }
+        }
+
+        return result;
+
     }
 };

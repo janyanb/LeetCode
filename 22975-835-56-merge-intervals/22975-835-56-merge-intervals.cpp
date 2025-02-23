b98412cd5feda5ee787df66fcf68250a0e2e6148
Janya NB <janya-cs060@dsce.edu.in>
2025-02-22 20:39:11 -0800

    Time: 396 ms, Memory: 23.7 MB - LeetHub

---
+++ 22975-835-56-merge-intervals/22975-835-56-merge-intervals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        for(int i = 0; i < n; i++){
+            bool swapped = false;
+            for(int j = 0; j < n - i - 1 ; j++){                              //-1 because we will compare j+1 in the loop
+                if(intervals[j+1][0] < intervals[j][0]){                      //bubble sort
+                    swap(intervals[j], intervals[j+1]);
+                    swapped = true;     
+                }
+            };
+            if(!swapped) break;            
+        }
+
+        vector<vector<int>> mergedIntervals;
+        int i;
+        for(i =0; i< n -1 ;i++){                                             // -1 because we will compare i+1 in loop                             
+            if(intervals[i+1][0] <= intervals[i][1]){
+                int minNum = min(intervals[i][0], intervals[i+1][0]);
+                int maxNum = max(intervals[i][1], intervals[i+1][1]);
+                intervals[i+1][0] = minNum;
+                intervals[i+1][1] = maxNum;
+            } else {
+                mergedIntervals.push_back(intervals[i]);
+            }
+        }
+        
+        mergedIntervals.push_back(intervals[n-1]);
+        return mergedIntervals;
+    }
+};
+

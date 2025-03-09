94219ba862826229fb284850e28dc88da28cd7f2
Janya NB <janya-cs060@dsce.edu.in>
2025-03-08 19:57:50 -0800

    Time: 141 ms (5.23%), Space: 19.5 MB (17.02%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<vector<int>> freqIndex(nums.size() + 1, vector<int>());       //size + 1-> if all the elements in the array are same
+
+        sort(nums.begin(), nums.end());
+
+        //count as index, n as value
+        for(int i = 0; i < nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i-1]){          //if repeating adjacent elements continue
+                continue;
+            }
+
+            int index = count(nums.begin(), nums.end(), nums[i]);      //count number of occurances 
+            freqIndex[index].push_back(nums[i]);                       //freq of num is index
+        }
+
+        vector<int> result;
+        for(int i = freqIndex.size() - 1; i >= 0 ; i--){
+            for(int j = 0; j < freqIndex[i].size(); j++){
+                result.push_back(freqIndex[i][j]);
+                if(result.size() == k) return result;
+            }
+        };
+
+    return result;  
+    }
+};

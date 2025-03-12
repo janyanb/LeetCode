7268e50a19693c88611d685e935301af7a5165a1
Janya NB <janya-cs060@dsce.edu.in>
2025-03-12 02:56:43 -0700

    Time: 0 ms (100%), Space: 7.8 MB (28.28%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,7 +4,7 @@ public:
         uint32_t res = 0;
 
         for(int i = 0; i < 32; i++){
-            uint32_t bit = (n >> i) & 1;
+            uint32_t bit = (n >> i) & 1;    //right shift does not return the discarded value, ith value is extracted by the & operator
             res = res | (bit << (31 - i));
         }
 

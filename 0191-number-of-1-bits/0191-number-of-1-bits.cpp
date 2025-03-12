a13113afdcf78722a429a8b16787f0ac25638a46
Janya NB <janya-cs060@dsce.edu.in>
2025-03-12 00:36:39 -0700

    Time: 0 ms (100%), Space: 8.6 MB (12.31%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int hammingWeight(int n) {
         vector<int> binaryN;
-        while(n!= 1){
+        while(n!= 0){
             if(n%2 == 1){
                 binaryN.push_back(1);
             } else binaryN.push_back(0);
             n = n/2;     
-        }
-        int hW = count(binaryN.begin(), binaryN.end(), 1);
+        }        
 
-        return hW + 1;       //reverse(binaryN.begin(), binaryN.end()) = binary rep of n
+        return count(binaryN.begin(), binaryN.end(), 1);        //reverse(binaryN.begin(), binaryN.end()) = binary rep of n
         
     }
 };

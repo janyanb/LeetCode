855357fc2e38f2baa0d4e98b60963b638082d5e7
Janya NB <janya-cs060@dsce.edu.in>
2025-03-24 15:09:36 -0700

    Time: 8 ms (41.42%), Space: 21.3 MB (12.56%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses, vector<int>());
+        for(auto edge : prerequisites){
+            graph[edge[0]].push_back(edge[1]);
+        }
+
+        vector<bool> visited(numCourses, false);
+        function<bool(int)> dfs = [&](int curr){
+            if(visited[curr]) return false;
+            //if(graph[curr].empty()) return true;
+
+            visited[curr] = true;
+            for(int nei: graph[curr]){
+                if(!dfs(nei)){
+                    return false;
+                }             
+            }
+            visited[curr] = false;
+            graph[curr].clear();
+            return true;
+        };
+
+        for(int i = 0; i < numCourses; i++){
+            if(!dfs(i)){
+                return false;     //to make sure all graphs are checked(there may be multiple courses which have prerequisites)
+            }
+        };
+        return true;
+    }
+};

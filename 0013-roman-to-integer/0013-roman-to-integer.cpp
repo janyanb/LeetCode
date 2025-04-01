62fa8724408496e6e881bef495d4b0a40c057d0e
Janya NB <janya-cs060@dsce.edu.in>
2025-04-01 02:10:21 -0700

    Time: 12 ms (17.75%), Space: 12.3 MB (64.29%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> SymbolMap;
+        SymbolMap['I'] = 1;
+        SymbolMap['V'] = 5;
+        SymbolMap['X'] = 10;
+        SymbolMap['L'] = 50;
+        SymbolMap['C'] = 100;
+        SymbolMap['D'] = 500;
+        SymbolMap['M'] = 1000;
+
+        int i = 0, tot = 0;
+        while(i< s.size()){
+            if(SymbolMap[s[i]] < SymbolMap[s[i+1]]){
+                tot += SymbolMap[s[i+1]] - SymbolMap[s[i]];
+                i+=2;
+            } else {
+                tot += SymbolMap[s[i]];
+                i++;
+            }
+        }
+        return tot;
+    }
+};

f9446293f1ebf99befbc33679790f1441abcb1cc
Janya NB <janya-cs060@dsce.edu.in>
2025-04-01 00:35:41 -0700

    Time: 11 ms (6.55%), Space: 112.5 MB (42.18%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        if(gas.size() == 1 && gas[0] >= cost[0] ) return 0;
+        // if(gas.size() == 1 && gas[0] >= cost[0] ) return 0;
 
-        int i = 0, j = 0;
-        int start = -1, tgas = 0;
-        while(i < gas.size()){
-            if(start == -1){
-                if(gas[i] > cost[i]){
-                start = i;
-                } else {
-                    i++;
-                }
-            } else {
-                tgas += gas[i];
-                tgas -= cost[i];
-                if (tgas >= 0) {
-                    i++;
-                    i = i%gas.size();
-                } else {
-                    i = start + 1;
-                    start = -1;
-                    tgas = 0;                    
-                }
-                if(i == start) return start;  
-            }            
+        // int i = 0, j = 0;
+        // int start = -1, tgas = 0;
+        // while(i < gas.size()){
+        //     if(start == -1){
+        //         if(gas[i] > cost[i]){
+        //         start = i;
+        //         } else {
+        //             i++;
+        //         }
+        //     } else {
+        //         tgas += gas[i];
+        //         tgas -= cost[i];
+        //         if (tgas >= 0) {
+        //             i++;
+        //             i = i%gas.size();
+        //         } else {
+        //             i = start + 1;
+        //             start = -1;
+        //             tgas = 0;                    
+        //         }
+        //         if(i == start) return start;  
+        //     }            
+        // }
+        // return -1;
+
+        int start = 0, TGas = 0, TCost = 0, tgas = 0;
+        for(int i =0; i < gas.size(); i++){
+            TGas += gas[i];
+            TCost += cost[i];
+            tgas += gas[i] - cost[i];
+
+            if(tgas < 0){
+                start = i + 1;               //works because TGas >= TCost;
+                tgas = 0;                
+            }
         }
-        return -1;
+        return TGas>= TCost ? start: -1;
     }
 };

825b5208bf3ebae7b5b768e71c1590207cd6a5ac
Janya NB <janya-cs060@dsce.edu.in>
2025-04-10 03:50:07 -0700

    Time: 4 ms (95.01%), Space: 14 MB (60.9%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        map<int,int> LowestSum;
+        // map<int,int> LowestSum;        
         int sum;
         sort(nums.begin(), nums.end());
+        int LowestSum = nums[1] + nums[2] + nums[3];
+        int Smallestdiff = INT_MAX;
+
         
         for(int i = 0; i< nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i+1, right = nums.size() -1;
             while(left<right){
                 sum = nums[i] + nums[left] + nums[right];
@@ -17,12 +21,17 @@ public:
                     return target;
                 }
 
+                
+                // LowestSum[diff] = sum; 
                 int diff = abs(target - sum);
-                LowestSum[diff] = sum; 
+                if(diff < Smallestdiff){
+                    Smallestdiff = diff;
+                    LowestSum = sum;
+                }
             }
 
         }
 
-        return LowestSum.begin()->second;
+        return LowestSum;
     }
 };

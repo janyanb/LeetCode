d39822d3cc7e66cfae101b5a6a658d5d68658f91
Janya NB <janya-cs060@dsce.edu.in>
2025-04-11 01:43:28 -0700

    Time: 7 ms (6.55%), Space: 16.4 MB (9.8%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> Subsequence;
+        vector<int> elements;
+
+        function<void(int,vector<int>)> rec = [&](int index, vector<int> Subseq) -> void {
+            if(index >= nums.size()){
+                if(!count(Subsequence.begin(),Subsequence.end(), Subseq)) Subsequence.push_back(Subseq);
+                return;
+            }
+
+            //take the index in subseq
+            Subseq.push_back(nums[index]);
+            rec(index + 1, Subseq);
+
+            //do not take the index in subseq
+            Subseq.pop_back();
+            rec(index +1, Subseq);
+        };
+
+        rec(0, elements);
+        return Subsequence;        
+    }
+};

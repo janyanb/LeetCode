c880a6227b1becf0e1eff6d1b6e7e5aa57226705
Janya NB <janya-cs060@dsce.edu.in>
2025-04-16 14:17:59 -0700

    Time: 7 ms (14.34%), Space: 14.4 MB (26.89%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private: vector<vector<int>> res;
+public:
+    void findCombinations(int ind, int sum, vector<int>& combination, vector<int>& candidates, int target){
+        if(ind > candidates.size() || sum > target) return;
+        if(sum == target){ 
+            res.push_back(combination);
+            return;
+        }
+
+        for(int i = ind; i<candidates.size(); i++){
+            if(i > ind && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            combination.push_back(candidates[i]);
+            findCombinations(i+1, sum + candidates[i], combination, candidates, target);
+            combination.pop_back();
+        }
+
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target){
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        findCombinations(0, 0, combination, candidates, target);
+
+        return res;
+    }
+};

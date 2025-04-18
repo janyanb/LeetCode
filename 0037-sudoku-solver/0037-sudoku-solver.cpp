899a48271d24a4487589f438b1c56cbf1a1355ca
Janya NB <janya-cs060@dsce.edu.in>
2025-04-18 02:44:11 -0700

    Time: 378 ms (49.74%), Space: 8.8 MB (63.74%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValid(int row, int col, char num, vector<vector<char>>& board){
+        for(int i = 0; i<9; i++){
+            if(board[row][i] == num) return false;
+
+            if(board[i][col] == num)return false;
+
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i%3] == num) return false;   //checks box
+        }
+
+        return true;
+    }
+    bool solve(vector<vector<char>>& board){
+        for(int i = 0; i<9; i++){
+            for (int j = 0; j<9; j++){
+                if(board[i][j] == '.'){
+                    for(char num = '1'; num <= '9'; num++){
+                        if(isValid(i, j, num, board)){
+                            board[i][j] = num;
+
+
+                            if(solve(board) == true){
+                                return true;
+                            } else {
+                            board[i][j] = '.';
+                            }
+
+                            }                        
+
+                        }
+
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};

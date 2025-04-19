d6ad91808a8daadd7a81b2ea253cbb48bfd66102
Janya NB <janya-cs060@dsce.edu.in>
2025-04-19 06:30:56 -0700

    Time: 39 ms (58.03%), Space: 62.6 MB (52.15%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void findComb(int ind, vector<int>& arr, vector<int>& comb, vector<vector<int>>& res, int k){
+        
+        if(comb.size() == k){
+            res.push_back(comb);
+            return;
+        }
+
+        if(ind >= arr.size()) return;
+
+        comb.push_back(arr[ind]);
+        findComb(ind + 1, arr, comb, res, k);
+
+        comb.pop_back();
+        findComb(ind + 1, arr, comb, res, k);
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> comb;
+        vector<int> arr;
+        for(int i = 1; i<=n; i++){
+            arr.push_back(i);
+            cout<<arr[i - 1];
+        }
+        findComb(0, arr, comb, res, k);
+
+        return res;
+    }
+};

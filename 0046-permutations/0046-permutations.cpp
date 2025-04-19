e42b08e5bbabafaa5cc373de5daa5e23be5a2197
Janya NB <janya-cs060@dsce.edu.in>
2025-04-19 04:15:23 -0700

    Time: 0 ms (100%), Space: 10.6 MB (69.95%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void findPerm(int ind, vector<int>& ds, vector<vector<int>>& res, vector<int>& nums){
+    void findPerm(vector<int>& ds, vector<vector<int>>& res, vector<int>& nums){
         if(ds.size() == nums.size()){
             res.push_back(ds);
             return;
@@ -10,14 +10,14 @@ public:
             auto it = find(ds.begin(), ds.end(), nums[i]);
             if(it != ds.end()) continue;
             ds.push_back(nums[i]);
-            findPerm(i+1, ds, res, nums);
+            findPerm(ds, res, nums);
             ds.pop_back();
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> ds;
-        findPerm(0, ds, res, nums);
+        findPerm(ds, res, nums);
 
         return res;
     }

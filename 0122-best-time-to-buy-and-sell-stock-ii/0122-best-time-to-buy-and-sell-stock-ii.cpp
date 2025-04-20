60808ff914a13235b04a32667409b382e4af769c
Janya NB <janya-cs060@dsce.edu.in>
2025-04-20 03:37:19 -0700

    Time: 0 ms (100%), Space: 17 MB (84.57%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int totalProfit = 0;
-
-        // Iterate through the prices array
-        for (int i = 1; i < prices.size(); i++) {
-            // Add the positive difference between consecutive days
-            if (prices[i] > prices[i - 1]) {
-                totalProfit += prices[i] - prices[i - 1];
+        int right = 1;
+        while(right< prices.size()){
+            if(prices[right] > prices[right - 1]){
+                totalProfit += prices[right] - prices[right - 1]; 
             }
+            right++;
         }
 
         return totalProfit;

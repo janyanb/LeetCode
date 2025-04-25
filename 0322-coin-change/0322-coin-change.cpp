92dcc93bd700a2fd98655c4cc64cd69a452f5253
Janya NB <janya-cs060@dsce.edu.in>
2025-04-25 04:27:02 -0700

    Time: 59 ms (16.33%), Space: 49.7 MB (22.12%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    const int INF = 1e9; 
+public:
+    int findComb(vector<int>& nums, int amount, int ind, long long total, vector<vector<int>>& dp){
+        if(amount == total) return 0;
+        if(total > amount) return INF;
+        if(ind == nums.size()) return INF;
+
+        int& ans = dp[ind][total];
+        if (ans != -1) return ans; 
+
+        int pick = 1 + findComb(nums, amount, ind, total + nums[ind], dp);
+        int notPick = findComb(nums, amount, ind + 1, total, dp);
+
+        return ans = min(pick, notPick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+
+        long long total = 0;
+        vector<vector<int>> dp(coins.size(), vector<int>(amount +1, -1));
+        int res = findComb(coins, amount, 0, total, dp);
+
+        return (res != INF)? res : -1;
+    }
+};

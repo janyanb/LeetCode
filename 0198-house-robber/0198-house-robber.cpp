0c4b6a15a8157f1d9a58456dca44c0657137abca
Janya NB <janya-cs060@dsce.edu.in>
2025-04-25 01:29:28 -0700

    Time: 1 ms (9.39%), Space: 10.6 MB (45.11%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canRob(int i, vector<int>& nums, vector<int>& dp){
+        if(i == 0) return nums[i];
+        if(i < 0) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int pick = canRob(i - 2, nums, dp) + nums[i];
+        int notPick = canRob(i -1, nums, dp) + 0;
+
+        return dp[i] = max(pick, notPick);
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        vector<int> dp(nums.size(), -1);
+
+        return canRob(nums.size() - 1, nums, dp);
+    }
+};

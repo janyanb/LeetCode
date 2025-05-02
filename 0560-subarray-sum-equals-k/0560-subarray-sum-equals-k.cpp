636c9ffceda6b87a42203db1181a49c55025bfcd
Janya NB <janya-cs060@dsce.edu.in>
2025-05-01 23:50:17 -0700

    Time: 50 ms (53.02%), Space: 45.4 MB (57.07%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int sum = 0, count = 0;
-        unordered_map<int,int> PrefixMap;
+        unordered_map<int,int> PrefixMap;       //map must remember how many times prefix value has appeared, every position it has appeared can form a diff subarray with the current position(i).
+        PrefixMap[0] = 1;
 
         for(int i = 0; i<nums.size(); i++){
             sum += nums[i];
-
-            if(sum == k) count++;
 
             if(PrefixMap.find(sum - k) != PrefixMap.end()){
                 count += PrefixMap[sum -k];
